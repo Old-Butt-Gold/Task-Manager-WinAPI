da98e5f2809de925e5cfee05279576fe239bb6b2
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2024-10-20 15:57:10 +0300

    feat: add new columns info

--- TaskManager/main.cpp
+++ TaskManager/main.cpp
@@ -3,6 +3,7 @@
 #endif
 
 #pragma comment(lib, "Comctl32.lib")
+#pragma comment(lib, "Psapi.lib")
 
 #define MainName L"Task Manager"
 #define SCREEN_WIDTH  GetSystemMetrics(SM_CXSCREEN)
@@ -22,6 +23,12 @@
 #include <wingdi.h>
 #include <string>
 #include "resource.h"
+#include <TlHelp32.h> // For process enumeration
+#include <Psapi.h>    // For process memory information
+#include <shellapi.h>
+#include <map>
+
+#include "info.h"
 
 HMENU CreateAppMenu() {
     HMENU hMenu = CreateMenu();
@@ -64,15 +71,17 @@ HWND CreateProcessListView(HWND hParent) {
     ImageList_SetBkColor(hImageList, CLR_NONE);
     
     HICON hIcon = LoadIcon((HINSTANCE)GetWindowLongPtr(hParent, GWLP_HINSTANCE), MAKEINTRESOURCE(IDI_MAINICON));  // Пример: загрузка системной иконки
+    ImageList_AddIcon(hImageList, LoadIcon(NULL, IDI_WINLOGO));
     ImageList_AddIcon(hImageList, hIcon);
+    
     DestroyIcon(hIcon);
     ListView_SetImageList(hListView, hImageList, LVSIL_SMALL);
 
     const std::wstring columnHeaders[] = {
-        L"Имя процесса", L"ID процесса", L"Состояние", L"Приоритет",
+        L"Имя процесса", L"ID процесса", L"ID родительского процесса", L"Приоритет",
         L"Загрузка ЦП", L"Загрузка Памяти", L"Время создания", L"Путь к файлу", L"Битность"
     };
-    int columnWidths[] = { 200, 150, 150, 150, 150, 190, 180, 250, 80 };
+    int columnWidths[] = { 200, 150, 150, 150, 150, 190, 180, 240, 80 };
 
     for (int i = 0; i < 9; i++) {
         lvColumn.mask = (i == 0) 
@@ -80,9 +89,10 @@ HWND CreateProcessListView(HWND hParent) {
                         : LVCF_TEXT | LVCF_WIDTH | LVCF_SUBITEM | LVCF_FMT;
         
         lvColumn.fmt = LVCFMT_CENTER;
+        lvColumn.cchTextMax = MAX_PATH;
         lvColumn.pszText = (LPWSTR)columnHeaders[i].c_str();
         lvColumn.cx = columnWidths[i];
-        lvColumn.iImage = 0;
+        lvColumn.iImage = 1;
         
         ListView_InsertColumn(hListView, i, &lvColumn);
     }
@@ -113,20 +123,155 @@ LRESULT CALLBACK ListViewProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam
         default:
             return CallWindowProc(oldListViewProcessesProc, hWnd, uMsg, wParam, lParam);
     }
+    
     return CallWindowProc(oldListViewProcessesProc, hWnd, uMsg, wParam, lParam);
+}
+
+std::wstring GetPriority(DWORD processId)
+{
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
+    if (hProcess)
+    {
+        DWORD priorityClass = GetPriorityClass(hProcess);
+        if (priorityClass == 0) {
+            CloseHandle(hProcess);
+            return L"Неизвестно";
+        }
+
+        std::wstring priorityString;
+        switch (priorityClass) {
+        case IDLE_PRIORITY_CLASS: priorityString = L"Низкий"; break;
+        case BELOW_NORMAL_PRIORITY_CLASS: priorityString = L"Ниже нормального"; break;
+        case NORMAL_PRIORITY_CLASS: priorityString = L"Нормальный"; break;
+        case ABOVE_NORMAL_PRIORITY_CLASS: priorityString = L"Выше нормального"; break;
+        case HIGH_PRIORITY_CLASS: priorityString = L"Высокий"; break;
+        case REALTIME_PRIORITY_CLASS: priorityString = L"Реального времени"; break;
+        default: priorityString = L"Неизвестно"; break;
+        }
+        
+        CloseHandle(hProcess);
+        return priorityString;
+    }
+    return L"Нет доступа";
+}
+
+void ChangeIcon(PROCESSENTRY32 pe32, HWND hListView, LVITEM& lvi, std::map<std::wstring, int>& iconMap)
+{
+    TCHAR fullPath[MAX_PATH];
+    DWORD processNameLength = sizeof(fullPath) / sizeof(TCHAR);
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pe32.th32ProcessID);
+    if (hProcess) {
+        if (QueryFullProcessImageName(hProcess, 0, fullPath, &processNameLength)) {
+            std::wstring processPath(fullPath);
+            auto it = iconMap.find(processPath);
+            if (it != iconMap.end())
+            {
+                lvi.iImage = it->second;
+            } else
+            {
+                HICON hIcon = ExtractIcon(NULL, fullPath, 0);
+                if (hIcon) {
+                    int imageIndex = ImageList_AddIcon(ListView_GetImageList(hListView, LVSIL_SMALL), hIcon);
+                    if (imageIndex != -1) {
+                        lvi.iImage = imageIndex;
+                        iconMap[processPath] = imageIndex;
+                    }
+                    DestroyIcon(hIcon);
+                }
+            }
+        }
+        CloseHandle(hProcess);
+        
+    }
+}
+
+bool Is64BitProcess(DWORD processID) {
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, processID);
+    if (hProcess) {
+        BOOL is64Bit = FALSE;
+        IsWow64Process(hProcess, &is64Bit);
+        CloseHandle(hProcess);
+        return is64Bit == FALSE;
+    }
+    return false;
+}
+
+std::wstring GetProcessTimeCreation(DWORD processId)
+{
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
+    if (hProcess) {
+        FILETIME creationTime, exitTime, kernelTime, userTime;
+        if (GetProcessTimes(hProcess, &creationTime, &exitTime, &kernelTime, &userTime)) {
+            SYSTEMTIME st;
+            FileTimeToSystemTime(&creationTime, &st);
+            st.wHour += 3; //For UTC
+            wchar_t timeString[100];
+            swprintf_s(timeString, 100, L"%02d/%02d/%04d %02d:%02d:%02d",
+            st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute, st.wSecond);
+            std::wstring result(timeString);
+            return result;
+        }
+        CloseHandle(hProcess);
+    }
+    return L"";
+}
+
+void PopulateProcessListView(HWND hListView, std::map<std::wstring, int>& iconMap)
+{
+    ListView_DeleteAllItems(hListView);
+
+    HANDLE hProcessSnap;
+    PROCESSENTRY32 pe32;
+    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
+    if (hProcessSnap == INVALID_HANDLE_VALUE)
+    {
+        return;
+    }
+
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+    if (Process32First(hProcessSnap, &pe32)) {
+        do {
+            LVITEM lvi = {0};
+            lvi.mask = LVIF_TEXT | LVIF_IMAGE;
+            lvi.pszText = (LPWSTR)pe32.szExeFile;
+            lvi.iImage = 0;
+            lvi.cchTextMax = MAX_PATH;
+            ChangeIcon(pe32, hListView, lvi, iconMap);
+
+            int itemIndex = ListView_InsertItem(hListView, &lvi);
+
+            ListView_SetItemText(hListView, itemIndex, 1, (LPWSTR)std::to_wstring(pe32.th32ProcessID).c_str()); // ID
+            std::wstring parent = pe32.th32ParentProcessID == 0 ? L"Главный процесс" : std::to_wstring(pe32.th32ParentProcessID);
+            ListView_SetItemText(hListView, itemIndex, 2, (LPWSTR)parent.c_str())
+            ListView_SetItemText(hListView, itemIndex, 3, (LPWSTR)GetPriority(pe32.th32ProcessID).c_str());
+            ListView_SetItemText(hListView, itemIndex, 6, (LPWSTR)GetProcessTimeCreation(pe32.th32ProcessID).c_str());
+            ListView_SetItemText(hListView, itemIndex, 8, Is64BitProcess(pe32.th32ProcessID) ? (LPWSTR)L"64-bit" : (LPWSTR)L"32-bit");
+
+            TCHAR fullPath[MAX_PATH];
+            DWORD processNameLength = sizeof(fullPath) / sizeof(TCHAR);
+            if (QueryFullProcessImageName(OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pe32.th32ProcessID), 0, fullPath, &processNameLength)) {
+                ListView_SetItemText(hListView, itemIndex, 7, (LPWSTR)fullPath);
+            }
+            
+
+        } while (Process32Next(hProcessSnap, &pe32));
+    }
+
+    CloseHandle(hProcessSnap);
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     static HWND hFontButton, hTabControl, hListViewProcesses;
-
+    static std::map<std::wstring, int> iconMap;
+    
     switch (uMsg)
     {
         case WM_PAINT:
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
-
+                
             FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
             EndPaint(hwnd, &ps);
         }
@@ -147,6 +292,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             SetMenu(hwnd, CreateAppMenu());
 
             hListViewProcesses = CreateProcessListView(hTabControl);
+            PopulateProcessListView(hListViewProcesses, iconMap);
             oldListViewProcessesProc = (WNDPROC)SetWindowLongPtr(hListViewProcesses, GWLP_WNDPROC, (LONG_PTR)ListViewProc);    
         }
         break;
@@ -193,6 +339,11 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 if (nmhdr->idFrom == IDC_TABCONTROL && nmhdr->code == TCN_SELCHANGE) {
                     int tabIndex = TabCtrl_GetCurSel(hTabControl);
                     ShowWindow(hListViewProcesses, tabIndex == 0 ? SW_SHOW : SW_HIDE);
+
+                    if (tabIndex == 0)
+                    {
+                        PopulateProcessListView(hListViewProcesses, iconMap);
+                    }
                 }
             }
             break;
@@ -207,6 +358,11 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 case ID_MENU_EXIT:
                 {
                     SendMessage(hwnd, WM_CLOSE, wParam, lParam);    
+                }
+                break;
+                case ID_MENU_ABOUT:
+                {
+                    MessageBox(hwnd, L"Task Manager App\nDeveloped by Krutko Andrey\nBSUIR 2024", L"About", MB_OK | MB_ICONINFORMATION);
                 }
                 break;
             }
